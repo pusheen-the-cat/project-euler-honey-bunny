33dc48c3c044e06cf68076953419b8a37c2e7cc2
pusheen-the-cat <andreajzou@gmail.com>
2019-09-10 21:44:02 -0700

    Add description to Problem 9
    Co-Authored-By: Cory Groom <corypgr@users.noreply.github.com>

--- src/main/java/projecteuler/honeybunny/Problem9.java
+++ src/main/java/projecteuler/honeybunny/Problem9.java
@@ -3,6 +3,18 @@ package projecteuler.honeybunny;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Special Pythagorean triplet
+ *
+ * Problem 9
+ * A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+ *
+ * a2 + b2 = c2
+ * For example, 32 + 42 = 9 + 16 = 25 = 52.
+ *
+ * There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+ * Find the product abc.
+ */
 public class Problem9 {
     public static void main(String[] args) {
         for (Triplet triplet : getAllCombinations()) {
